590231fc53f083ee1b1a9a46eb7569f13896db0b
errasti <errasti007@yandex.ru>
2022-08-03 16:48:06 +0300

    Initial commit

---
+++ hw_01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numOne = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numSec = int.Parse(Console.ReadLine());
+int result = 1;
+
+for (int i = 1; i<=numSec; i++)
+{
+    result = result * numOne;
+}
+Console.WriteLine("Первое число в степени второго = " + result);
+
