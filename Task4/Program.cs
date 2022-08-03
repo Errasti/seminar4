c36600551fad210261bac97b3d0042a73886c3f7
errasti <errasti007@yandex.ru>
2022-08-03 11:27:28 +0300

    Initial commit

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int i=1; i<=a; i++)
+{
+    result = result * i;
+}
+Console.WriteLine(result);
