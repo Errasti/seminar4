e760694e1c4737feb11530031e497791c05aa879
errasti <errasti007@yandex.ru>
2022-08-03 16:59:12 +0300

    Initial commit

---
+++ hw_02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int userNumber = int.Parse(Console.ReadLine());
+int summ = 0;
+while( userNumber != 0 )
+{
+    summ = summ + userNumber % 10;
+    userNumber = userNumber / 10;
+}
+Console.WriteLine(summ);
