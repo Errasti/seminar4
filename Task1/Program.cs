1119eb95dca345da076aea23ff2d702b99ed33db
errasti <errasti007@yandex.ru>
2022-08-03 10:49:08 +0300

    Initial Commit

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число a:");
+int a = int.Parse(Console.ReadLine());
+int numbersSum = 0;
+
+for(int i = 1; i <= a; i++)
+{
+    numbersSum = numbersSum + i;
+}
+Console.WriteLine(numbersSum);
+
