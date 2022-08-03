a7ef15f3baffa30c1c5577decfc7a0c4bcc8b548
errasti <errasti007@yandex.ru>
2022-08-03 17:17:59 +0300

    Initial commit

---
+++ hw_03/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] fillArray(int n)
+{
+    int[] array = new int[n];
+    Random rnd = new Random();
+    for( int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100,100);
+    }
+    
+    return array;
+}
+
+Console.WriteLine("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int [] arr = fillArray(size);
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
